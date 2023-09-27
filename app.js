4b82f0369e5ae7fa9e768d9f7eb3d08d819e606b
JrSchoolAccount <jesperx2000@gmail.com>
2023-09-27 20:17:32 +0200

    Major update

--- app.js
+++ app.js
@@ -5,28 +5,40 @@ const btn = document.querySelector('#submitTask')
 const info = document.querySelector('small');
 const completedInfo = document.querySelector('p');
 
-// Variables
-let completedCount = 0;
+// Variable
+let completedCounter = 0;
 
-// Array containing objects with Todo tasks
+// Array objects with Todo tasks
 const todoArray = [];
+
+// Function to handle change status of Object
+// Takes parameter completed (bool)
+function changeStatus(inputTask, completed) {
+
+  // find index, look in object and value on "name"
+  let correctIndex = todoArray.map(t => t.name).indexOf(inputTask);
+
+  // Change status of object to correct index
+  todoArray[correctIndex].status = completed;
+}
 
 // Function
 btn.addEventListener('click',function() {
   // Fetch input value
-  const text = inputTask.value + ' ';
-
-  // Add todo-object to todoArray
-  todoArray.push(text);
+  const text = inputTask.value;
 
   // Check that input is not empty
   if(text.length == 0){
-    info.innerText = "Input can not be empty";
+    info.innerText = 'Input can not be empty!';
     return;
   }
   else{
     info.innerText = '';
   }
+
+    // Add todo to todoArray
+    const todoObj = {name: text, status: false};
+    todoArray.push(todoObj);
 
   // Create a new li-element in ul
   const item = document.createElement('li');
@@ -36,31 +48,38 @@ btn.addEventListener('click',function() {
   const itemLabel = document.createElement('span');
   itemLabel.innerText = text;
   item.appendChild(itemLabel);
-
-  // Add listener to span and change completedCount
-  itemLabel.addEventListener('click',function(){
-
-    // Toggle completed/uncompleted task
-
-    if (item.getAttribute('class') == 'completed'){
-      item.setAttribute('class', '');
-      completedCount--
-    }
-
-    else {
-      item.setAttribute('class', 'completed');
-      completedCount++
-    }
-
-    completedInfo.innerText = `${completedCount} completed`;
-
-  })
-
+  
   // Add trashcan icon to span element
   const trashcan = document.createElement('span');
   trashcan.setAttribute('class', 'trashcan');
   trashcan.setAttribute('class', 'fa-solid fa-trash-can-arrow-up fa-lg');
   item.appendChild(trashcan);
+
+  // Add listener to span and change completedCounter/ True-False
+  itemLabel.addEventListener('click',function(){
+
+    // Toggle completed/uncompleted task
+    if (item.getAttribute('class') == 'completed'){
+      item.setAttribute('class', '');
+
+      // Change status of object to False
+      let clickedText = item.firstChild.firstChild.textContent;
+      changeStatus(clickedText, false);
+      completedCounter--
+    }
+
+    else {
+      item.setAttribute('class', 'completed');
+
+      // Change status of object to True
+      clickedText = item.firstChild.firstChild.textContent;
+      changeStatus(clickedText, true);
+      completedCounter++
+    }
+
+    completedInfo.innerText = `${completedCounter} completed`;
+
+  })
 
 /*  Add listener to span to delete item on click.
     With conditionals for editing total completed count.
@@ -68,15 +87,23 @@ btn.addEventListener('click',function() {
   trashcan.addEventListener('click', function () {
     if (item.getAttribute('class') == 'completed'){
       // Remove item from todoArray
+      let removeText = item.firstChild.firstChild.textContent;
+      let indexToRemove = todoArray.map(t => t.name).indexOf(removeText);
+      todoArray.splice(indexToRemove, 1);
+      
       item.remove();
-      completedCount--
+      completedCounter--
     }
     else {
       // Remove item from todoArray
+      removeText = item.firstChild.firstChild.textContent;
+      let indexToRemove = todoArray.map(t => t.name).indexOf(removeText);
+      todoArray.splice(indexToRemove, 1);
+
       item.remove();
     }
 
-    completedInfo.innerText = `${completedCount} completed`;
+    completedInfo.innerText = `${completedCounter} completed`;
 
   });
 
